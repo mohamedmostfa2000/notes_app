163b41b201bd6f12da0c048802916e64f7779a4a
mohamed.mostfa1970 <mohamed.mostfa1970@gmail.com>
2022-11-09 22:08:48 +0200

    mohamed mostfa - AppBar

--- lib/main.dart
+++ lib/main.dart
@@ -12,8 +12,8 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-       theme: ThemeData(brightness: Brightness.dark),
-       home: const NotesView(),
-          );
+      theme: ThemeData(brightness: Brightness.dark),
+      home: const NotesView(),
+    );
   }
 }
